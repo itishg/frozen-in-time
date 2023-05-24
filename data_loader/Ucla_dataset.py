2fb807be2e45687bf65cc6169351ae0978fb2da7
Itish Goel <goelitish@gmail.com>
2023-05-24 15:01:23 -0400

    Update Ucla_dataset.py

--- data_loader/Ucla_dataset.py
+++ data_loader/Ucla_dataset.py
@@ -7,7 +7,7 @@ from base.base_dataset import TextImageDataset
 class Ucla(TextImageDataset):
     def _load_metadata(self):
         csv_fp = os.path.join(self.metadata_dir, 'labels_split.csv')  # Load labels_split.csv
-        df = pd.read_csv(csv_fp, sep='\t', on_bad_lines='skip')
+        df = pd.read_csv(csv_fp, sep='\t')
 
         train_df = df[df['split'] == 'train']  # Get rows where 'split' is 'train'
         test_df = df[df['split'] == 'test']  # Get rows where 'split' is 'test'
